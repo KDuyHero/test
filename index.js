eb25c67dc89ce2f91c17314161e4bcc4147be131
KDuyHero <dtrang1570@gmail.com>
2023-07-13 20:21:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { getAllBrands } = require("./src/controller/brandController");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected");
+  } catch (error) {
+    console.log("Connect to db fail");
+  }
+};
+
+app.get("/", (req, res) => {
+  res.json("hello world");
+});
+
+app.get("/brands", getAllBrands);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`running on ${PORT}`);
+  });
+});
---
+++ src/controller/brandController.js
@@ -0,0 +1,103 @@
+const brandModel = require("../models/BrandModel");
+
+const getAllBrands = async (req, res) => {
+  try {
+    const brands = await brandModel.find();
+    return res.status(200).json({
+      success: true,
+      message: "OK",
+      brands,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some thing went wrong in get all Brands",
+      error,
+    });
+  }
+};
+const createBrand = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(200).json("Name is required");
+    const newBrand = new brandModel({ name });
+    await newBrand.save();
+    return res.status(200).json({
+      success: true,
+      message: "OK",
+      newBrand,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some thing went wrong in create brand",
+      error,
+    });
+  }
+};
+const getBrand = async (req, res) => {
+  try {
+    const { brandId } = req.params;
+    const foundBrand = await brandModel.findById(brandId);
+    return res.statsu(200).json({
+      success: true,
+      message: "OK",
+      brand: foundBrand,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some thing went wrong in get brand",
+      error,
+    });
+  }
+};
+const updateBrand = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { brandId } = req.params;
+    const foundBrand = await brandModel.findOne({ name });
+    if (foundBrand) return res.status(200).json("Tên brand này đã tồn tại");
+    const updateBrand = await brandModel.findByIdAndUpdate(brandId, { name });
+    return res.status(200).json({
+      success: true,
+      message: "OK",
+      updateBrand,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some thing went wrong in update brand",
+      error,
+    });
+  }
+};
+const deleteBrand = async (req, res) => {
+  try {
+    const { brandId } = req.params;
+    const foundBrand = await brandModel.findById(brandId);
+
+    if (!foundBrand) return res.status(200).json("brand không tồn tại");
+
+    const deleteBrand = await brandModel.findByIdAndDelete(brandId);
+    return res.status(200).json({
+      success: true,
+      message: "OK",
+      deleteBrand,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Some thing went wrong in delete brand",
+      error,
+    });
+  }
+};
+
+module.exports = {
+  getAllBrands,
+  createBrand,
+  getBrand,
+  updateBrand,
+  deleteBrand,
+};
