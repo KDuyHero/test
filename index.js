2639ecd9ba698b64dee407449d89e956f2024032
KDuyHero <dtrang1570@gmail.com>
2023-07-14 07:10:26 +0700

    add cors and public dir

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const { getAllBrands } = require("./src/controller/brandController");
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 const connectDB = async () => {
@@ -17,6 +18,10 @@ const connectDB = async () => {
 app.get("/", (req, res) => {
   res.json("hello world");
 });
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./src/public"));
 
 const Router = require("./src/routes");
 
